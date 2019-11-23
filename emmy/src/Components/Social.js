7901be59e19f6cb357af4505c2330b24c5b42228
damian garcia <dexequiel.garcia@gmail.com>
2019-11-24 11:27:29 +1300

    Social decision partial commit

--- emmy/src/Components/Social.js
+++ emmy/src/Components/Social.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { Fab } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete'
+import TextField from '@material-ui/core/TextField';
 
-function Social(props) {
-    return <h1>Social</h1>;
+class Social extends Component {
+    
+  constructor(
+    props
+  ) {
+    super(props);
+    this.state = {
+      options : [],
+      newOption : ""
+    };
+    this.addOption = this.addOption.bind(this);
   }
+
+  render() {
+    let {options} = this.state;
+      return <Fragment>
+          <h1>Social</h1>
+          <Grid container>
+            <Grid item>
+            <label>Type your question</label>
+            
+            <br/>
+            <TextField id="question" label="question" multiline/>
+            </Grid>
+
+            <br/>
+            <label>Type your options</label>
+            {
+              options.length ? options.map(el => 
+                <span value={el} key={el}> {el}
+                <Fab color="primary" aria-label="add">
+                  <DeleteIcon />
+                </Fab>
+                </span>
+                ) 
+                : <span>No options listed</span>
+            }
+
+            <br/>
+            <TextField id="new-option" 
+              label="Type an option" 
+              value={this.state.newOption} 
+              onChange={ this.typeNewOption.bind(this) }
+              //onChangeText={this.typeNewOption.bind(this)}
+              />
+            <br/>
+            
+            <Fab color="primary" aria-label="add" onClick={(e) => { this.addOption(e) }}>
+              <AddIcon ></AddIcon>
+            </Fab>
+          </Grid>
+        </Fragment>
+      ;
+    }
+
+    addOption(e) {
+      e.preventDefault();
+      // let {state} = this.state;
+      this.state.options.push(this.state.newOption);
+      this.state.newOption = "";
+    }
+
+    typeNewOption(e) {
+      //e.preventDefault();
+      this.setState({ newOption: e.target.value });
+    }
+}
 
 export default Social;
